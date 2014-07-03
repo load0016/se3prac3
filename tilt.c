863c41d07461763e20444999a5605704849b4d77
load0016 <load0016@flinders.edu.au>
2014-07-03 17:59:57 +0930

    Created slide method

--- tilt.c
+++ tilt.c
@@ -7,6 +7,18 @@ int tilt_line_left(int length,int *line)
   if (length<1||length>255) return -1;
 
   // slide tiles to the left
+  int a;
+  int b;
+  
+  for(b=0; i<length; b++) {
+    if(line[b]!=0) {
+      if(i!=a) {
+        line[a]=line[i];
+        line[i]=0;
+      }
+    }
+  }
+  
 
   // combine tiles as required
 
