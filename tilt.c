e7cc77d9389e92c87a338533d78c198b2c01bb86
load0016 <load0016@flinders.edu.au>
2014-07-03 18:37:59 +0930

    Final comments added, all tests passing.
    Fixed formatting, spaced out code for easier reading.

--- tilt.c
+++ tilt.c
@@ -6,7 +6,7 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  // slide tiles to the left
+  // slide tiles to the left - all tests passing.
   int a=0;
   int b=0;
   int c=0;
@@ -14,19 +14,25 @@ int tilt_line_left(int length,int *line)
   for(b=0; b<length; b++) {
     if(line[b]!=0) {
       if(b!=a) {
+        
         line[a]=line[b];
         line[b]=0;
       }
       a++;
     }
   }
-   // combine tiles as required
+  
+   // combine tiles as required - all tests passing.
    for(c=0; c<length; c++) {
-     if (line[c]==line[c+1]&&line[c]!=0) {
+     
+     if (line[c] == line[c+1] && line[c]!=0) {
        line[c]+=line[c];
+       
+       //moving values across
        int d=0;
        for (d=c+1; d<length; d++) {
-          line[d]=line[d+1];
+         
+          line[d] = line[d+1];
        }
        line[d]=0;
      } 
